427fcd65b17556e0776501811e67d997a392274b
Magnus Mårtensson <magnus@loftysoft.com>
2016-09-05 21:23:29 +0200

    Working with Linked Templates.

---
+++ 04LinkedTemplates/DeployTemplate.ps1
@@ -0,0 +1,18 @@
+﻿Set-Location "$demosPath\LinkedTemplates"
+
+$resourceGroupName = "ARMTrainingLinked"
+$templateFile = ".\azuredeploy.json"
+$templateParametersFile = ".\azuredeploy.parameters.json"
+
+New-AzureRmResourceGroup `
+    -Name $resourceGroupName `
+    -Location $Location.Location `
+    -Verbose -Force
+
+New-AzureRmResourceGroupDeployment `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile $templateFile `
+    -Verbose -Force
+    -TemplateParameterFile $templateParametersFile
+
+RemoveResourceGroup
