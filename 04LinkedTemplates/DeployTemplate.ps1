4b9a6149de3897bd7183707d1154220381aace01
Magnus Mårtensson <magnus@loftysoft.com>
2016-09-05 22:38:35 +0200

    Linked almost works

--- 04LinkedTemplates/DeployTemplate.ps1
+++ 04LinkedTemplates/DeployTemplate.ps1
@@ -2,7 +2,13 @@
 
 $resourceGroupName = "ARMTrainingLinked"
 $templateUri = "https://raw.githubusercontent.com/noopman/Azure-Resource-Manager-Wizardry/master/04LinkedTemplates/azuredeploy.json"
+$templateUri = "https://armtraining42.blob.core.windows.net/armdemo/azuredeploy.1.json"
+$templateUri = "https://armtraining42.blob.core.windows.net/armdemo/azuredeploy.json"
 $templateParametersUri = "https://raw.githubusercontent.com/noopman/Azure-Resource-Manager-Wizardry/master/04LinkedTemplates/azuredeploy.parameters.json"
+$templateParametersUri = "https://armtraining42.blob.core.windows.net/armdemo/azuredeploy.parameters.json"
+
+$templateUri = "https://armtraining42.blob.core.windows.net/armdemo/azuredeploy.1.json"
+$templateParametersUri = "https://armtraining42.blob.core.windows.net/armdemo/azuredeploy.parameters.json"
 
 New-AzureRmResourceGroup `
     -Name $resourceGroupName `
